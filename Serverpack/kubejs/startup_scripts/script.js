8349ae0505145743aeab4361758a3114c6683c21
AlbisMinerva <snipcar48@gmail.com>
2024-06-07 23:36:08 +0900

    fix

--- Serverpack/kubejs/startup_scripts/script.js
+++ Serverpack/kubejs/startup_scripts/script.js
@@ -12,23 +12,3 @@ onEvent('block.registry', event => {
 	// Register new blocks here
 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
 })
-
-
-// onEvent('recipes', event => {
-//	event.remove({output: 'pixelmon:white_healer'})
-//	event.remove({output: 'pixelmon:orange_healer'})
-//	event.remove({output: 'pixelmon:magenta_healer'})
-//	event.remove({output: 'pixelmon:light_blue_healer'})
-//	event.remove({output: 'pixelmon:yellow_healer'})
-//	event.remove({output: 'pixelmon:lime_healer'})
-//	event.remove({output: 'pixelmon:pink_healer'})
-//	event.remove({output: 'pixelmon:gray_healer'})
-//	event.remove({output: 'pixelmon:light_gray_healer'})
-//	event.remove({output: 'pixelmon:cyan_healer'})
-//	event.remove({output: 'pixelmon:purple_healer'})
-//	event.remove({output: 'pixelmon:blue_healer'})
-//	event.remove({output: 'pixelmon:brown_healer'})
-//	event.remove({output: 'pixelmon:green_healer'})
-//	event.remove({output: 'pixelmon:red_healer'})
-//	event.remove({output: 'pixelmon:black_healer'})
-//})
